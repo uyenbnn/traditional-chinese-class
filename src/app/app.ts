24f18b8153ad88eade2f54c90ea9388e6767b666
uyenbnn <nuyenbui2004763@gmail.com>
2026-04-14 12:01:28 +0700

    refactor css

--- src/app/app.ts
+++ src/app/app.ts
@@ -14,6 +14,7 @@ import { AuthService } from './services/auth.service';
 })
 export class App {
   readonly authService = inject(AuthService);
+  readonly currentYear = new Date().getFullYear();
   private readonly router = inject(Router);
 
   prepareRoute(outlet: RouterOutlet): string {
